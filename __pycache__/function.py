7866e3ea62eb0ac45da4a29605475b234d6db985
beniman8 <benik@live.ca>
2022-07-17 23:00:07 -0400

    functions

---
+++ __pycache__/function.py
@@ -0,0 +1,19 @@
+
+
+# def hell_world():
+
+#     return "Hello World"
+
+
+
+# print(hell_world())
+
+name = input("what is your name")
+
+def what_is_my_name(name):
+
+    return name
+
+
+
+print(what_is_my_name(name))
